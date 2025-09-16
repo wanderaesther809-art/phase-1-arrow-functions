0122b336a76d113a7852b3306e23df81de7e2834
Esther Wandera <wanderaesther809@gmail.com>
2025-09-17 01:17:45 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function (a, b) {
+  return a / b;
+};
+
+console.log(divide(2000, 100));
+
+const square = (x) => x * x;
+console.log(square(2));
+
+const add = (a, b) => a + b;
+console.log(add(3, 4));
