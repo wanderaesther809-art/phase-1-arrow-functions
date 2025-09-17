6cedf4a19bb7e8f3529a43f713ecc1c7ddba3b97
Esther Wandera <wanderaesther809@gmail.com>
2025-09-17 03:07:51 +0300

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const divide = function (a, b) {
-  return a / b;
-};
+function receivesAFunction(callback) {
+  callback();
+}
 
-console.log(divide(2000, 100));
+console.log("Testing receivesAFunction:");
+receivesAFunction(() => console.log("Callback was called"));
 
-const square = (x) => x * x;
-console.log(square(2));
+function returnsANamedFunction() {
+  function namedFunction() {
+    return "i am named!";
+  }
+  return namedFunction;
+}
 
-const add = (a, b) => a + b;
-console.log(add(3, 4));
+console.log("\nTesting returnsANamedFunction:");
+const named = returnsANamedFunction();
+console.log("Is it a function? ", typeof named === "function");
+
+function returnsAnAnonymousFunction() {
+  return function () {
+    return "i am anonymous";
+  };
+}
+
+console.log("\nTesting returnsAnAnonymousFunction:");
+const anon = returnsAnAnonymousFunction();
+console.log("Is it a function? ", typeof anon === "function");
+console.log("Function name:", anon.name);
+console.log("Calling it: ", anon());
